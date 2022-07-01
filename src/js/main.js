7211a3a18d47ba730a9d01b01c5d90cf72281902
saramonzon22 <sara.mon.quesada@gmail.com>
2022-07-01 12:53:01 +0100

    first objt.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
 'use strict';
 
+const nameJs = document.querySelector ('.js-name-input');
+const jobJs = document.querySelector ('.js-job-input');
+const imgJs = document.querySelector ('.js-img-input');
+const phoneJs = document.querySelector ('.js-phone-input');
+const emailJs = document.querySelector ('.js-email-input');
+const linkJs = document.querySelector ('.js-link-input');
+const gitJs = document.querySelector ('.js-git-input');
+const form = document.querySelector ('.form');
+const previewName = document.querySelector('.js-preview-name');
+const previewJob = document.querySelector('.js-preview-job');
+const previewLink = document.querySelector('.js-preview-phone');
+const previewPhoto = document.querySelector('.js-preview-photo');
 
-const collapsableMenu = document.querySelectorAll('.collapsablemenu');
+
+/* const collapsableMenu = document.querySelectorAll('.collapsablemenu');
 collapsableMenu.forEach(item => item.addEventListener('click', hideShow));
 
 function hideShow (event) {
@@ -18,6 +31,28 @@ function hideShow (event) {
       }
     });
   }
-}
+} */
 
 
+
+function listen (event) {
+  const dataCard = {
+    palette: 1,
+    name: nameJs.value,
+    job: jobJs.value,
+    phone: phoneJs.value,
+    email: emailJs.value,
+    linkedin: linkJs.value,
+    github: gitJs.value,
+    photo: imgJs.value,
+  };
+  previewName.innerHTML = nameJs.value;
+  previewJob.innerHTML = jobJs.value;
+  previewPhoto.src = imgJs.value;
+  previewLink.href = linkJs.value;
+
+};
+
+
+
+form.addEventListener('keyup', listen);
