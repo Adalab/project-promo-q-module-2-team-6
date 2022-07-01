bde4a16a3d968f13888633b4af06a60ba1a277d7
lupeMorales <morales.lupegmail.com>
2022-07-01 12:35:12 +0200

    fix conflicts

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
+const collapsableMenu = document.querySelectorAll(".collapsablemenu");
+collapsableMenu.forEach((item) => item.addEventListener("click", hideShow));
 
-const collapsableMenu = document.querySelectorAll('.collapsablemenu');
-collapsableMenu.forEach(item => item.addEventListener('click', hideShow));
-
-function hideShow (event) {
+function hideShow(event) {
   const fieldsetElement = event.currentTarget;
-  const sectionToHide = fieldsetElement.querySelector('.section-to-hide'); //
-  const arrowSection = fieldsetElement.querySelector('.arrow');
-  sectionToHide.classList.toggle('collapsed');
-  arrowSection.classList.toggle('rotate');
 
-  if (!fieldsetElement.classList.contains('collapsed')) {
+  const sectionToHide = fieldsetElement.querySelector(".section-to-hide"); //
+  const arrowSection = fieldsetElement.querySelector(".arrow");
+  sectionToHide.classList.toggle("collapsed");
+  arrowSection.classList.toggle("rotate");
+
+  if (!fieldsetElement.classList.contains("collapsed")) {
     collapsableMenu.forEach((section) => {
       if (section !== fieldsetElement) {
-        section.querySelector('.section-to-hide').classList.add('collapsed');
+        section.querySelector(".section-to-hide").classList.add("collapsed");
       }
     });
   }
