36a304be4a4367faf352a82c0f7916145fc33734
SandMl3 <sandramartinlop@gmail.com>
2022-07-04 12:32:28 +0200

    Solving profile image bug

--- src/js/color_preview.js
+++ src/js/color_preview.js
@@ -54,9 +54,8 @@ function getImage (e) {
 
 
 function writeImage () {
-
-  profileImage.style.backgroundImage = `url(${newFile.result})`;
-  profilePreview.style.backgroundImage = `url(${newFile.result})`;
+  profileImage.style.background = `url(${newFile.result})`;
+  profilePreview.src = newFile.result;
 }
 
 
