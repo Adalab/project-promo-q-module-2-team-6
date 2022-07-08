32ccb50b3802fb6d6e71971ac5bbf51bae0dc9dd
SandMl3 <sandramartinlop@gmail.com>
2022-07-08 13:00:30 +0200

    Solving some bugs with reset buttons and image preview

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 
 //---------------------------------------Funcionalidad FORMULARIO INTERACTIVO
@@ -22,6 +22,9 @@ const emailJs = document.querySelector('.js-email-input');//Email.
 const linkJs = document.querySelector('.js-link-input');//Linkedin.
 const gitJs = document.querySelector('.js-git-input');//GitHub.
 const phoneJs = document.querySelector('.js-preview-phone');//Phone.
+
+//Elementos del FORMULARIO. IMAGEN.
+const profileImage = document.querySelector('.js__profile-image');
 
 
 
@@ -79,6 +82,7 @@ function cardDefault() {
 
 //Resetea el formulario.
 function resetForm() {
+
   dataCard.palette = 1;
   nameJs.value = '';
   jobJs.value = '';
@@ -87,10 +91,14 @@ function resetForm() {
   emailJs.value = '';
   linkJs.value = '';
   gitJs.value = '';
+  profileImage.style.background = `url(${dataCard.photo})`;
+  profileImage.style.backgroundSize = 'cover';
+  profileImage.style.backgroundPosition = 'center';
 }
 
 //Devuelve la tarjeta a su estado original.
 function resetCard() {
+
   cardDefault();
   previewName.innerHTML = dataCard.name;
   previewJob.innerHTML = dataCard.job;
@@ -99,13 +107,12 @@ function resetCard() {
   previewPhone.href = `tel: ${dataCard.phone}`;
   previewLink.href = dataCard.linkedin;
   previewGithub.href = dataCard.github;
-  profileImage.style.background = `url(${dataCard.photo})`;
-  profilePreview.src = dataCard.photo;
 }
 
 
 //Función manejadora del botón RESET.
 function handleClickReset() {
+  cardDefault();
   resetForm();
   resetCard();
 }
@@ -114,52 +121,11 @@ resetBtn.addEventListener('click', handleClickReset);
 
 
 
-/* //--------------------------Código funcionalidad MENÚS COLAPSABLES (sin objetos).
-/* const formElement = document.querySelector('.form');
+//------------------------------------Código funcionalidad MENÚS COLAPSABLES.
 
-const localizeFieldset = (clickedElement) => clickedElement.classList.contains('js-legend') ? clickedElement : clickedElement.parentElement;
-
-const arrowPositioner = (sectionClicked, collapsableSection) => {
-  !collapsableSection.classList.contains('collapsed') ?sectionClicked.querySelector('.arrow').classList.add('rotate') : sectionClicked.querySelector('.arrow').classList.remove('rotate');
-};
-
-const menuCollapser = (sectionClicked)=> {
-  const collapsableSection = sectionClicked.parentElement.querySelector('.section-to-hide');
-  collapsableSection.classList.toggle('collapsed');
-  arrowPositioner(sectionClicked, collapsableSection);
-
-  if (!collapsableSection.classList.contains('collapsed')) {
-
-    if (collapsableSection.classList.contains('fill-inputs-section')) {
-      document.querySelector('.container-collapsible').classList.add('collapsed');
-      document.querySelector('.design-colors').classList.add('collapsed');
-
-    } else if (collapsableSection.classList.contains('design-colors')) {
-      document.querySelector('.container-collapsible').classList.add('collapsed');
-      document.querySelector('.fill-inputs-section').classList.add('collapsed');
-
-    } else if (collapsableSection.classList.contains('container-collapsible')) {
-      document.querySelector('.design-colors').classList.add('collapsed');
-      document.querySelector('.fill-inputs-section').classList.add('collapsed');
-    }
-  }
-};
-
-const handleFunction = (event) => {
-  const clickedElement = event.target;
-  if (clickedElement.classList.contains('js-legend') || clickedElement.parentElement.classList.contains('js-legend')) {
-    const sectionClass = localizeFieldset(clickedElement);
-    menuCollapser(sectionClass);
-  }
-};
-
-formElement.addEventListener('click', handleFunction); */
-
-//--------------------------Código funcionalidad MENÚS COLAPSABLES CON OBJETOS.
-
-const collapsableMenu = document.querySelectorAll(".collapsablemenu"); //Con querySelectoAll() objetemos un objeto con todos los elementos que tienen el selector especificado (en este caso la clase 'collapsablemenu')
+const collapsableMenu = document.querySelectorAll('.collapsablemenu'); //Con querySelectoAll() objetemos un objeto con todos los elementos que tienen el selector especificado (en este caso la clase 'collapsablemenu')
 collapsableMenu.forEach((item) =>
-  item.addEventListener("click", hideShow, true)
+  item.addEventListener('click', hideShow, true)
 );
 //forEahc() va a ir recorriendo el objeto propiedad por propiedad. Lo que ponemos entre paréntesis es el nombre que le daremos a cada propiedad para referirnos a ellas dentro de la función flecha. En este caso le estamos diciendo que por cada item (propiedad) añada un eventListener.
 //Ponemos a la escucha cada una de las secciones que hay dento del form y establecemos que al hacer click en ellas se ejecute la funcion manejadorea hideshow.
@@ -186,24 +152,24 @@ const createCard = (event, element) => {
 
 const arrowPositioner = () => {
   collapsableMenu.forEach((section) =>
-    !section.querySelector(".section-to-hide").classList.contains("collapsed")
-      ? section.querySelector(".arrow").classList.add("rotate")
-      : section.querySelector(".arrow").classList.remove("rotate")
+    !section.querySelector('.section-to-hide').classList.contains('collapsed')
+      ? section.querySelector('.arrow').classList.add('rotate')
+      : section.querySelector('.arrow').classList.remove('rotate')
   );
 }; //Con esta función volvemos a recorrer los parámetros del objeto (es decir, los tres fieldsets) con un condicional (ternario) cuyo objetivo es rotar la flecha si la sección del fieldset está oculta.
 
 function menuCollapser(fieldsetElement) {
-  const sectionToHide = fieldsetElement.querySelector(".section-to-hide");
-  sectionToHide.classList.toggle("collapsed");
+  const sectionToHide = fieldsetElement.querySelector('.section-to-hide');
+  sectionToHide.classList.toggle('collapsed');
 } //Con esta función estamos colapsando el menú si detecta un click y está descolapsado y viceversa.
 
 const otherMenusCollapser = (fieldsetElement) => {
   collapsableMenu.forEach((item) => {
     if (
-      !item.querySelector(".section-to-hide").classList.contains("collapsed") &&
+      !item.querySelector('.section-to-hide').classList.contains('collapsed') &&
       item !== fieldsetElement
     ) {
-      item.querySelector(".section-to-hide").classList.add("collapsed");
+      item.querySelector('.section-to-hide').classList.add('collapsed');
     }
   });
 }; //Aquí estamos colapsando el resto de los menús cuándo uno de ellos se abre. Para ello estamos recorriendo de nuevo el objeto y por cada una de sus propiedades (item) estamos comprobando si hay alguna que no tenga la clase collapsed (es decir, que esté desplegada) y que no sea la misma que está siendo clickada en este momento. Si eso se da, lo que hacemos es añadirle la clase collapsed para que la única que quede desplegada sea la que acabamos de abrir.
@@ -218,7 +184,7 @@ function hideShow(event) {
 
   if (
     fieldsetElement === clickedElement ||
-    clickedElement.parentElement.classList.contains("js-legend")
+    clickedElement.parentElement.classList.contains('js-legend')
   ) {
     menuCollapser(fieldsetElement);
     otherMenusCollapser(fieldsetElement);
@@ -229,87 +195,3 @@ function hideShow(event) {
   paletteSelection(clickedElement);
   createCard(event, clickedElement);
 }
-<<<<<<< HEAD
-
-//---------------------------------------Funcionalidad FORMULARIO INTERACTIVO
-
-const nameJs = document.querySelector(".js-name-input");
-const jobJs = document.querySelector(".js-job-input");
-const imgJs = document.querySelector(".js-img-input");
-const phoneJs = document.querySelector(".js-phone-input");
-const emailJs = document.querySelector(".js-email-input");
-const linkJs = document.querySelector(".js-link-input");
-const gitJs = document.querySelector(".js-git-input");
-const form = document.querySelector(".form");
-const previewName = document.querySelector(".js-preview-name");
-const previewJob = document.querySelector(".js-preview-job");
-const previewPhone = document.querySelector(".js-preview-phone");
-const previewLink = document.querySelector(".js-preview-link");
-const previewEmail = document.querySelector(".js-preview-email");
-const previewGithub = document.querySelector(".js-preview-github");
-const previewPhoto = document.querySelector(".js-preview-photo");
-const resetBtn = document.querySelector(".js-resetBtn");
-
-const dataCard = {
-  palette: 1,
-  name: "",
-  job: "",
-  phone: "",
-  email: "",
-  linkedin: "",
-  github: "",
-  photo: "",
-};
-
-//previsualiza datos en tarjeta
-function listen() {
-  previewName.innerHTML = nameJs.value;
-  previewJob.innerHTML = jobJs.value;
-  previewPhoto.src = imgJs.value;
-  previewEmail.href = emailJs.value;
-  previewPhone.href = `tel: ${phoneJs.value}`;
-  previewLink.href = linkJs.value;
-  previewGithub.href = gitJs.value;
-}
-
-form.addEventListener("keyup", listen);
-
-function cardDefault() {
-  dataCard.palette = 1;
-  dataCard.name = "Nombre Apellido";
-  dataCard.job = "Front developer";
-  dataCard.phone = "";
-  dataCard.email = "";
-  dataCard.linkedin = "";
-  dataCard.github = "";
-  dataCard.photo = "./assets/images/photo.png";
-  return dataCard;
-}
-
-//----------------------------------------------------RESETEANDO EL FORMULARIO al clickar RESET.
-//init form
-function resetForm() {
-  nameJs.value = "";
-  jobJs.value = "";
-  imgJs.src = "";
-  phoneJs.value = "";
-  emailJs.value = "";
-  linkJs.value = "";
-  gitJs.value = "";
-}
-//init card
-function resetCard() {
-  cardDefault();
-  previewName.innerHTML = dataCard.name;
-  previewJob.innerHTML = dataCard.job;
-  previewPhoto.src = dataCard.photo;
-  previewLink.href = dataCard.linkedin;
-}
-
-function handleClickReset() {
-  resetForm();
-  resetCard();
-}
-resetBtn.addEventListener("click", handleClickReset);
-=======
->>>>>>> 41f5a1c84aa664777a2930380528b4c18f6e128e
