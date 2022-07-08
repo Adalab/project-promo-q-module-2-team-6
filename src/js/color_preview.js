03027ab0b263debfcc6a506b3246906842f1a2b0
saramonzon22 <sara.mon.quesada@gmail.com>
2022-07-08 11:46:39 +0100

    fix

--- src/js/color_preview.js
+++ src/js/color_preview.js
@@ -1,25 +1,25 @@
-"use strict";
+'use strict';
 
 // -------------------- Preparación inicial div img ----------------
 
 //parte imagen
 const newFile = new FileReader();
-const fileField = document.querySelector(".js__profile-upload-btn");
-const profileImage = document.querySelector(".js__profile-image");
-const profilePreview = document.querySelector(".js-preview-photo");
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js-preview-photo');
 
 //@param { evento } e
 function getImage(e) {
   const myFile = e.currentTarget.files[0];
-  newFile.addEventListener("load", writeImage);
+  newFile.addEventListener('load', writeImage);
   newFile.readAsDataURL(myFile);
 }
 
 function writeImage() {
   profileImage.style.background = `url(${newFile.result})`;
-  profileImage.style.backgroundSize = "cover";
+  profileImage.style.backgroundSize = 'cover';
   profilePreview.src = newFile.result;
   dataCard.photo = newFile.result;
 }
 
-fileField.addEventListener("change", getImage);
+fileField.addEventListener('change', getImage);
