b650056c2d50d51221bb96c93667fec93c705f73
Sandra Martín <sandramartin@MacBook-Pro-de-Sandra.local>
2022-07-13 21:15:35 +0200

    Solving some bugs

--- src/js/main.js
+++ src/js/main.js
@@ -248,24 +248,3 @@ function handleCreatedCard(ev) {
 create.addEventListener('click', handleCreatedCard);
 
 
-// -------------------- Preparación inicial div img ----------------
-
-//parte imagen
-const newFile = new FileReader();
-const fileField = document.querySelector('.js__profile-upload-btn');
-
-//@param { evento } e
-function getImage(e) {
-  const myFile = e.currentTarget.files[0];
-  newFile.addEventListener('load', writeImage);
-  newFile.readAsDataURL(myFile);
-}
-
-function writeImage() {
-  profileImage.style.background = `url(${newFile.result})`;
-  profileImage.style.backgroundSize = 'cover';
-  profilePreview.src = newFile.result;
-  dataCard.photo = newFile.result;
-}
-
-fileField.addEventListener('change', getImage);
--- src/js/preview-color.js
+++ src/js/preview-color.js
@@ -1,3 +1,23 @@
 'use strict';
 
+// -------------------- Preparación inicial div img ----------------
 
+//parte imagen
+const newFile = new FileReader();
+const fileField = document.querySelector('.js__profile-upload-btn');
+
+//@param { evento } e
+function getImage(e) {
+  const myFile = e.currentTarget.files[0];
+  newFile.addEventListener('load', writeImage);
+  newFile.readAsDataURL(myFile);
+}
+
+function writeImage() {
+  profileImage.style.background = `url(${newFile.result})`;
+  profileImage.style.backgroundSize = 'cover';
+  profilePreview.src = newFile.result;
+  dataCard.photo = newFile.result;
+}
+
+fileField.addEventListener('change', getImage);
