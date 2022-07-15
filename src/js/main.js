9a4ad3154b58a9ec767238e83bb9e90797480468
SandMl3 <sandramartinlop@gmail.com>
2022-07-15 07:31:57 +0200

    Run docs

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,47 @@
-"use strict";
+'use strict';
 
 //---------------------------------------Funcionalidad FORMULARIO INTERACTIVO
 //Elementos de la TARJETA.
-const previewName = document.querySelector(".js-preview-name"); //Name.
-const previewJob = document.querySelector(".js-preview-job"); //Job.
-const previewPhone = document.querySelector(".js-preview-phone"); //Phone.
-const previewLink = document.querySelector(".js-preview-link"); //Linkedin.
-const previewEmail = document.querySelector(".js-preview-email"); //Email.
-const previewGithub = document.querySelector(".js-preview-github"); //GitHub.
-const previewPhoto = document.querySelector(".js-preview-photo"); //Photo.
-const resetBtn = document.querySelector(".js-resetBtn"); //Elemento botón RESET.
-const form = document.querySelector(".form"); //Elemnto formulario.
+const previewName = document.querySelector('.js-preview-name'); //Name.
+const previewJob = document.querySelector('.js-preview-job'); //Job.
+const previewPhone = document.querySelector('.js-preview-phone'); //Phone.
+const previewLink = document.querySelector('.js-preview-link'); //Linkedin.
+const previewEmail = document.querySelector('.js-preview-email'); //Email.
+const previewGithub = document.querySelector('.js-preview-github'); //GitHub.
+const previewPhoto = document.querySelector('.js-preview-photo'); //Photo.
+const resetBtn = document.querySelector('.js-resetBtn'); //Elemento botón RESET.
+const form = document.querySelector('.form'); //Elemnto formulario.
 
 //Elementos del FORMULARIO.
-const nameJs = document.querySelector(".js-name-input"); //Name.
-const jobJs = document.querySelector(".js-job-input"); //Job.
-const imgJs = document.querySelector(".js-img-input"); //Img.
-const emailJs = document.querySelector(".js-email-input"); //Email.
-const linkJs = document.querySelector(".js-link-input"); //Linkedin.
-const gitJs = document.querySelector(".js-git-input"); //GitHub.
-const phoneJs = document.querySelector(".js-phone-input"); //Phone.
-const linkCard = document.querySelector(".link");
-const create = document.querySelector(".button-create");
-const profileImage = document.querySelector(".js__profile-image");
-const profilePreview = document.querySelector(".js-preview-photo");
-const collapsableMenu = document.querySelectorAll(".collapsablemenu");
-const buttonShare = document.querySelector(".button-share");
+const nameJs = document.querySelector('.js-name-input'); //Name.
+const jobJs = document.querySelector('.js-job-input'); //Job.
+const imgJs = document.querySelector('.js-img-input'); //Img.
+const emailJs = document.querySelector('.js-email-input'); //Email.
+const linkJs = document.querySelector('.js-link-input'); //Linkedin.
+const gitJs = document.querySelector('.js-git-input'); //GitHub.
+const phoneJs = document.querySelector('.js-phone-input'); //Phone.
+const linkCard = document.querySelector('.link');
+const create = document.querySelector('.button-create');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js-preview-photo');
+const collapsableMenu = document.querySelectorAll('.collapsablemenu');
+const buttonShare = document.querySelector('.button-share');
 
 //Definimos el objeto dataCard.
 const dataCard = {
   palette: 1,
-  name: "Nombre Apellido",
-  job: "Front-End developer",
-  phone: "",
-  email: "",
-  linkedin: "",
-  github: "",
-  photo: "./assets/images/photo2.png",
+  name: 'Nombre Apellido',
+  job: 'Front-End developer',
+  phone: '',
+  email: '',
+  linkedin: '',
+  github: '',
+  photo: './assets/images/photo2.png',
 };
 
 const defaultCheckedPalette = (dataCard) => {
   const paletteDefault = document.getElementById(`${dataCard.palette}`);
-  paletteDefault.setAttribute("checked", "");
+  paletteDefault.setAttribute('checked', '');
 };
 
 defaultCheckedPalette(dataCard);
@@ -56,7 +56,7 @@ const htmlPreview = (dataCard) => {
   previewLink.href = dataCard.linkedin;
   previewGithub.href = dataCard.github;
   profileImage.style.background = `url(${dataCard.photo})`;
-  profileImage.style.backgroundSize = "cover";
+  profileImage.style.backgroundSize = 'cover';
   profilePreview.src = dataCard.photo;
 };
 
@@ -72,33 +72,33 @@ function handlePreviewData(event) {
   htmlPreview(dataCard);
 }
 
-form.addEventListener("keyup", handlePreviewData);
+form.addEventListener('keyup', handlePreviewData);
 
 //-----------------------------------------------------------------Botón RESET.
 
 function cardDefault() {
   dataCard.palette = 1;
-  dataCard.name = "Nombre Apellido";
-  dataCard.job = "Front developer";
-  dataCard.phone = "";
-  dataCard.email = "";
-  dataCard.linkedin = "";
-  dataCard.github = "";
-  dataCard.photo = "./assets/images/photo2.png";
+  dataCard.name = 'Nombre Apellido';
+  dataCard.job = 'Front developer';
+  dataCard.phone = '';
+  dataCard.email = '';
+  dataCard.linkedin = '';
+  dataCard.github = '';
+  dataCard.photo = './assets/images/photo2.png';
 }
 
 function resetForm() {
   dataCard.palette = 1;
-  nameJs.value = "";
-  jobJs.value = "";
-  imgJs.src = "";
-  phoneJs.value = "";
-  emailJs.value = "";
-  linkJs.value = "";
-  gitJs.value = "";
+  nameJs.value = '';
+  jobJs.value = '';
+  imgJs.src = '';
+  phoneJs.value = '';
+  emailJs.value = '';
+  linkJs.value = '';
+  gitJs.value = '';
   profileImage.style.background = `url(${dataCard.photo})`;
-  profileImage.style.backgroundSize = "cover";
-  profileImage.style.backgroundPosition = "center";
+  profileImage.style.backgroundSize = 'cover';
+  profileImage.style.backgroundPosition = 'center';
 }
 
 function resetCard() {
@@ -111,24 +111,24 @@ function resetCard() {
   previewLink.href = dataCard.linkedin;
   previewGithub.href = dataCard.github;
   profileImage.style.background = `url(${dataCard.photo})`;
-  profileImage.style.backgroundSize = "cover";
-  profileImage.style.backgroundPosition = "center";
+  profileImage.style.backgroundSize = 'cover';
+  profileImage.style.backgroundPosition = 'center';
   profilePreview.src = dataCard.photo;
 }
 
-const resetLocalStorage = () => localStorage.removeItem("userData");
+const resetLocalStorage = () => localStorage.removeItem('userData');
 
 const collapseCreateCardSection = () => {
   if (
-    document.querySelector(".button-create").classList.contains("inactived")
+    document.querySelector('.button-create').classList.contains('inactived')
   ) {
-    document.querySelector(".button-create").classList.remove("inactived");
+    document.querySelector('.button-create').classList.remove('inactived');
   }
-  if (!document.querySelector(".created").classList.contains("collapsed")) {
-    document.querySelector(".created").classList.add("collapsed");
+  if (!document.querySelector('.created').classList.contains('collapsed')) {
+    document.querySelector('.created').classList.add('collapsed');
   }
-  if (document.querySelector(".check").classList.contains("collapsed")) {
-    document.querySelector(".check").classList.add("collapsed");
+  if (document.querySelector('.check').classList.contains('collapsed')) {
+    document.querySelector('.check').classList.add('collapsed');
   }
 };
 
@@ -139,14 +139,14 @@ function handleClickReset() {
   resetLocalStorage();
 }
 
-resetBtn.addEventListener("click", handleClickReset);
+resetBtn.addEventListener('click', handleClickReset);
 
 //---------------------------------------------------------- MENUS COLAPSABLES.
 
 const paletteSelection = (element) => {
-  const card = document.querySelector(".card");
-  const cardStyles = ["palette1", "palette2", "palette3", "palette4"];
-  if (element.name === "palette") {
+  const card = document.querySelector('.card');
+  const cardStyles = ['palette1', 'palette2', 'palette3', 'palette4'];
+  if (element.name === 'palette') {
     dataCard[element.name] = parseInt(element.id);
     cardStyles.forEach((item) =>
       item === `palette${element.id}`
@@ -158,39 +158,39 @@ const paletteSelection = (element) => {
 
 const displayCreateCardSection = (event, element) => {
   if (
-    element.name === "newCardButton" ||
-    element.parentElement.name === "newCardButton"
+    element.name === 'newCardButton' ||
+    element.parentElement.name === 'newCardButton'
   ) {
     event.preventDefault();
     nameJs.value && jobJs.value && emailJs.value && gitJs.value && linkJs.value
-      ? document.querySelector(".created").classList.remove("collapsed")
-      : document.querySelector(".check").classList.remove("collapsed");
-    if (!document.querySelector(".created").classList.contains("collapsed")) {
-      document.querySelector(".button-create").classList.add("inactived");
+      ? document.querySelector('.created').classList.remove('collapsed')
+      : document.querySelector('.check').classList.remove('collapsed');
+    if (!document.querySelector('.created').classList.contains('collapsed')) {
+      document.querySelector('.button-create').classList.add('inactived');
     }
   }
 };
 
 const arrowPositioner = () => {
   collapsableMenu.forEach((section) =>
-    !section.querySelector(".section-to-hide").classList.contains("collapsed")
-      ? section.querySelector(".skull").classList.add("rotate")
-      : section.querySelector(".skull").classList.remove("rotate")
+    !section.querySelector('.section-to-hide').classList.contains('collapsed')
+      ? section.querySelector('.skull').classList.add('rotate')
+      : section.querySelector('.skull').classList.remove('rotate')
   );
 };
 
 function menuCollapser(fieldsetElement) {
-  const sectionToHide = fieldsetElement.querySelector(".section-to-hide");
-  sectionToHide.classList.toggle("collapsed");
+  const sectionToHide = fieldsetElement.querySelector('.section-to-hide');
+  sectionToHide.classList.toggle('collapsed');
 }
 
 const otherMenusCollapser = (fieldsetElement) => {
   collapsableMenu.forEach((item) => {
     if (
-      !item.querySelector(".section-to-hide").classList.contains("collapsed") &&
+      !item.querySelector('.section-to-hide').classList.contains('collapsed') &&
       item !== fieldsetElement
     ) {
-      item.querySelector(".section-to-hide").classList.add("collapsed");
+      item.querySelector('.section-to-hide').classList.add('collapsed');
     }
   });
 };
@@ -200,7 +200,7 @@ function handleFunctionCollapse(event) {
   const fieldsetElement = event.currentTarget;
   if (
     fieldsetElement === clickedElement ||
-    clickedElement.parentElement.classList.contains("js-legend")
+    clickedElement.parentElement.classList.contains('js-legend')
   ) {
     menuCollapser(fieldsetElement);
     otherMenusCollapser(fieldsetElement);
@@ -211,23 +211,23 @@ function handleFunctionCollapse(event) {
 }
 
 collapsableMenu.forEach((item) =>
-  item.addEventListener("click", handleFunctionCollapse)
+  item.addEventListener('click', handleFunctionCollapse)
 );
 
 //--------------------------------------------------------LOCAL STORAGE.
 
 function saveDataLocalStorage() {
-  localStorage.setItem("userData", JSON.stringify(dataCard));
+  localStorage.setItem('userData', JSON.stringify(dataCard));
 }
 
 function getDataLocalStorage() {
-  const dataLocalStorage = JSON.parse(localStorage.getItem("userData"));
+  const dataLocalStorage = JSON.parse(localStorage.getItem('userData'));
   if (dataLocalStorage !== null) {
     nameJs.value = dataLocalStorage.name;
     jobJs.value = dataLocalStorage.job;
     profileImage.style.background = `url(${dataLocalStorage.photo})`;
-    profileImage.style.backgroundSize = "cover";
-    profileImage.style.backgroundPosition = "center";
+    profileImage.style.backgroundSize = 'cover';
+    profileImage.style.backgroundPosition = 'center';
     emailJs.value = dataLocalStorage.email;
     linkJs.value = dataLocalStorage.linkedin;
     gitJs.value = dataLocalStorage.github;
@@ -242,10 +242,10 @@ getDataLocalStorage();
 //--------------------------------------------------------CREAR TARJETA.
 
 function apiCall() {
-  fetch("https://awesome-profile-cards.herokuapp.com/card", {
-    method: "POST",
+  fetch('https://awesome-profile-cards.herokuapp.com/card', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(dataCard),
   })
@@ -257,7 +257,7 @@ function apiCall() {
 
         buttonShare.href = `https://twitter.com/intent/tweet?text=Hello%20world%20mi%20tarjeta&url=${serverResponse.cardURL}`;
       } else {
-        linkCard.innerHTML = "Error";
+        linkCard.innerHTML = 'Error';
       }
     })
     .catch((error) => {
@@ -271,4 +271,4 @@ function handleCreatedCard(ev) {
   saveDataLocalStorage();
 }
 
-create.addEventListener("click", handleCreatedCard);
+create.addEventListener('click', handleCreatedCard);
